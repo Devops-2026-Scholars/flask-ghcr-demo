3306fd729f70617818b56d879ac02a7fd66c44c8
olufrancis <temi.olumoko@gmail.com>
2026-09-06 23:04:10 +0100

    Ensure that app runs on local 127.0.0.1

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return jsonify({"message": "Hello from Github Actions & GHCR!", "version": "1.0.0"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000)
+    app.run(host='127.0.0.1', port=6000)
